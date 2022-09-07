d233afb34f2dcf7f4be4bfc80790e7c71f6c9983
Alejandro Mendoza <aletinmendoza@hotmail.com>
2022-09-07 15:44:03 -0600

    SEM-12 create models and migrations

---
+++ semillita/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from app.models import *
+# Register your models here.
+admin.site.register(Planta)
+admin.site.register(Seccion)
+admin.site.register(Uso)
+admin.site.register(Imagen)
+admin.site.register(Analiticos)
