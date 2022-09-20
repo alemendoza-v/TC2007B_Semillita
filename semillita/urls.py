85432784be12bdea3189ba95c6701767d64c1a2e
Alejandro Mendoza <aletinmendoza@hotmail.com>
2022-09-20 16:40:27 -0600

    SEM-41 Added Create API and Read (All plants) API

--- semillita/urls.py
+++ semillita/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from app import views
+
+router = routers.DefaultRouter()
+router.register(r'plantas', views.PlantaViewSet, basename='plantas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
