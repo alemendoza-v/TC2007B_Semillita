295a0747417fb97a37b9ead6d2828bbb559f3e2d
Alejandro Mendoza <aletinmendoza@hotmail.com>
2022-09-23 18:49:54 -0600

    No me odie profe

--- app/views.py
+++ app/views.py
@@ -1,15 +1,13 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from app.models import Planta, Imagen, Analiticos, Uso
 from app.serializers import PlantaSerializer, ImagenSerializer, UsoSerializer
 
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 import base64
 
 # This class is a viewset that allows us to create, retrieve, update, and delete Usos objects.
@@ -43,7 +41,9 @@ class PlantaViewSet(viewsets.ModelViewSet):
             "temporada": "",
             "estatus": true,
             "descripcion": "",
-            "f
+            "fertilizante": "",
+            "riego": "",
+            "iluminacion": "",
         """
         data = request.data
         try:
@@ -64,7 +64,7 @@ class PlantaViewSet(viewsets.ModelViewSet):
                 uso = Uso.objects.get(id=u)
                 new_plant.usos.add(uso)
             new_plant.save()
-            return Response("Planta creada", status=status.HTTP_201_CREATED)
+            return HttpResponse("Planta creada", status=status.HTTP_201_CREATED)
         except:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
