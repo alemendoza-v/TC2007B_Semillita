83cb8351d7e2932bd60f15e16be9979ab5541465
Ale Mendoza <aletinmendoza@hotmail.com>
2022-10-08 10:58:28 -0600

    SEM-80 Finished Tests

--- app/tests/test_usos.py
+++ app/tests/test_usos.py
@@ -1,58 +1,30 @@
-from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
 
-from app.views import UsosViewSet
-from app.models import Uso
-
 from django.contrib.auth.models import User
+
 import json
+import os
 class TestUsoViewSet(APITestCase):
     def setUp(self):
-        # Create a user.
+        # GIVEN
         self.user = User.objects.create_user(
-            username='Test',
-            password='Test',
-            email='Test',
-            first_name='Test',
-            last_name='Test',
+            username='Goldie',
+            password='testing1928',
+            email=os.getenv('EMAIL_USER'),
         )
         self.user.save()
-        self.factory = APIRequestFactory()
         self.uri = '/api/usos/'
-        self.view = UsosViewSet.as_view( {'get': 'list', 'post': 'create', 'delete': 'destroy'} )
         self.data = {
-            'username': 'Test',
-            'password': 'Test',
+            'username': 'Goldie',
+            'password': 'testing1928',
         }
         self.data = json.dumps(self.data)
         self.token = self.client.post('/api/token/', self.data, content_type="application/json")
         self.token = json.loads(self.token.content)['access']
 
     def test_list(self):
-        request = self.factory.get(self.uri, HTTP_AUTHORIZATION="Bearer " + self.token)
-        response = self.view(request)
-        self.assertEqual(response.status_code, 200, 'Expected Response Code 200, received {0} instead.'.format(response.status_code))
+        # WHEN
+        request = self.client.get(self.uri, HTTP_AUTHORIZATION="Bearer " + self.token)
+        # THEN
+        self.assertEqual(request.status_code, 200, 'Expected Response Code 200, received {0} instead.'.format(request.status_code))
         print("Uso list test passed")
-
-    def test_create(self):
-        data = {
-            'nombre': 'Test',
-        }
-        request = self.factory.post(self.uri, data, HTTP_AUTHORIZATION="Bearer " + self.token)
-        response = self.view(request)
-        self.assertEqual(response.status_code, 201, 'Expected Response Code 201, received {0} instead.'.format(response.status_code))
-        print("Uso create test passed")
-
-    def test_retrieve(self):
-        uso = Uso.objects.create(nombre='Test')
-        request = self.factory.get(self.uri, HTTP_AUTHORIZATION="Bearer " + self.token)
-        response = self.view(request, pk=uso.pk)
-        self.assertEqual(response.status_code, 200, 'Expected Response Code 200, received {0} instead.'.format(response.status_code))
-        print("Uso retrieve test passed")
-
-    def test_delete(self):
-        uso = Uso.objects.create(nombre='Test')
-        request = self.factory.delete(self.uri, HTTP_AUTHORIZATION="Bearer " + self.token)
-        response = self.view(request, pk=uso.pk)
-        self.assertEqual(response.status_code, 204, 'Expected Response Code 204, received {0} instead.'.format(response.status_code))
-        print("Uso delete test passed")
